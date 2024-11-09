ffa0635b951fe5969560242dabd1807be4e05103
916-Racsan-Rares <82086767+RaresRacsan@users.noreply.github.com>
2024-11-09 17:40:37 +0200

    testing

---
+++ client_app.c
@@ -0,0 +1,103 @@
+#include <gtk/gtk.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 8080
+#define CHUNK_SIZE 16384
+
+GtkWidget *entry_server_ip;
+GtkWidget *entry_file_name;
+GtkWidget *label_status;
+
+void request_file (const char *serverIp, const char *fileName) {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = inet_addr(serverIp);
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        gtk_label_set_text(GTK_LABEL(label_status), "Connection failed.");
+        closesocket(clientSocket);
+        WSACleanup();
+        return;
+    }
+
+    // Send filename request
+    send(clientSocket, fileName, strlen(fileName), 0);
+
+    // Receive file size
+    long fileSize;
+    recv(clientSocket, (char*)&fileSize, sizeof(fileSize), 0);
+
+    // Receive file data
+    FILE *file = fopen(fileName, "wb");
+    char buffer[CHUNK_SIZE];
+    int bytesReceived;
+    while ((bytesReceived = recv(clientSocket, buffer, CHUNK_SIZE, 0)) > 0) {
+        fwrite(buffer, 1, bytesReceived, file);
+    }
+    fclose(file);
+
+    gtk_label_set_text(GTK_LABEL(label_status), "File received successfully.");
+    closesocket(clientSocket);
+    WSACleanup();
+}
+
+// Callback when Request button is clicked
+void on_request_button_clicked (GtkWidget *widget, gpointer data) {
+    const char *serverIp = gtk_entry_get_text(GTK_ENTRY(entry_server_ip));
+    const char *fileName = gtk_entry_get_text(GTK_ENTRY(entry_file_name));
+
+    if(strlen(serverIp) == 0 || strlen(fileName) == 0) {
+        gtk_label_set_text(GTK_LABEL(label_status), "Enter server IP and file name.");
+        return;
+    } 
+
+    gtk_label_set_text(GTK_LABEL(label_status), "Requesting file...");
+    request_file(serverIp, fileName);
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Client Application");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    GtkWidget *label_ip = gtk_label_new("Server IP:");
+    entry_server_ip = gtk_entry_new();
+    GtkWidget *label_file = gtk_label_new("File Name:");
+    entry_file_name = gtk_entry_new();
+    label_status = gtk_label_new("");
+
+    gtk_box_pack_start(GTK_BOX(vbox), label_ip, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), entry_server_ip, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), label_file, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), entry_file_name, FALSE, FALSE, 0);
+
+    GtkWidget *button_request = gtk_button_new_with_label("Request File");
+    gtk_box_pack_start(GTK_BOX(vbox), button_request, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), label_status, FALSE, FALSE, 0);
+
+    g_signal_connect(button_request, "clicked", G_CALLBACK(on_request_button_clicked), NULL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+
+    gtk_main();
+
+    return 0;
+}
---
+++ server_app.c
@@ -0,0 +1,126 @@
+#include <gtk/gtk.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 8080
+#define CHUNK_SIZE 16384
+
+GtkWidget *label_status;
+GtkWidget *label_file_requested;
+
+SOCKET serverSocket;
+SOCKET clientSocket;
+
+// Function to send the file if the request is accepted
+void send_file(const char *fileName) {
+    FILE *file = fopen(fileName, "rb");
+    if(file == NULL) {
+        gtk_label_set_text(GTK_LABEL(label_status), "File not found.");
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Send the file size
+    send(clientSocket, (char*) &fileSize, sizeof(fileSize), 0);
+
+    // Send file data in chunks
+    char buffer[CHUNK_SIZE];
+    int bytesRead;
+    while ((bytesRead = fread(buffer, 1, CHUNK_SIZE, file)) > 0) {
+        send(clientSocket, buffer, bytesRead, 0);
+    }
+
+    fclose(file);
+    gtk_label_set_text(GTK_LABEL(label_status), "File sent successfully.");
+}
+
+// Callback when Accept button is clicked
+void on_accept_button_clicked(GtkWidget *widget, gpointer data) {
+    const char *fileName = gtk_label_get_text(GTK_LABEL(label_file_requested));
+    send_file(fileName);
+}
+
+// Callback when Decline button is clicked
+void on_decline_button_clicked(GtkWidget *widget, gpointer data) {
+    gtk_label_set_text(GTK_LABEL(label_status), "File request declined.");
+    closesocket(clientSocket);
+}
+
+void start_server() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket, (const struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, 3);
+
+    gtk_label_set_text(GTK_LABEL(label_status), "Server listening for requests...");
+
+    struct sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+
+    char fileName[256];
+    recv(clientSocket, fileName, sizeof(fileName), 0);
+    gtk_label_set_text(GTK_LABEL(label_file_requested), fileName);
+    gtk_label_set_text(GTK_LABEL(label_status), "File request received.");
+}
+
+int main(int argc, char *argv[]) {
+    gtk_init(&argc, &argv);
+
+    // Create the main window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Server application");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+
+    // Create a vertical box layout
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    // Status and requested file labels
+    label_status = gtk_label_new("Starting server...");
+    label_file_requested = gtk_label_new("No file requested.");
+    gtk_box_pack_start(GTK_BOX(vbox), label_status, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), label_file_requested, FALSE, FALSE, 0);
+
+    // Accept and decline buttons
+    GtkWidget *button_accept = gtk_button_new_with_label("Accept");
+    GtkWidget *button_decline = gtk_button_new_with_label("Decline");
+    gtk_box_pack_start(GTK_BOX(vbox), button_accept, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(vbox), button_decline, FALSE, FALSE, 0);
+
+    // Connect button signals to their callbacks
+    g_signal_connect(button_accept, "clicked", G_CALLBACK(on_accept_button_clicked), NULL);
+    g_signal_connect(button_decline, "clicked", G_CALLBACK(on_decline_button_clicked), NULL);
+
+    // Show all widgets
+    gtk_widget_show_all(window);
+
+     // Start the server in a separate thread to listen for requests
+    g_idle_add((GSourceFunc)start_server, NULL);
+
+    // Signal to close the window
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_main();
+
+    closesocket(serverSocket);
+    WSACleanup();
+
+    return 0;
+}
